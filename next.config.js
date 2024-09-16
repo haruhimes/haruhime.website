ff181f946317a5bbf7ca4bebb464a68a6981bc05
Haruhime <140860369+haruhimes@users.noreply.github.com>
2024-09-15 21:16:52 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'avatars1.githubusercontent.com',
+                port: '',
+                pathname: '/u/**',
+            },
+        ],
+    }
+};
