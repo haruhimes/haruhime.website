8c23f851e706646da563e1c49a6f53b8699db55f
Haruhime <140860369+haruhimes@users.noreply.github.com>
2024-09-21 00:43:14 -0700

    Update SocialSection.tsx

--- src/app/container/SocialSection.tsx
+++ src/app/container/SocialSection.tsx
@@ -1,44 +1,43 @@
 import React from "react";
-
 import Icon from "@component/svg/Icon";
 
 interface Social {
-  Icon  : React.ComponentType<any>;
-  label : string;
-  color : string;
-  link  : string;
+    Icon: React.ComponentType<any>;
+    label: string;
+    color: string;
+    link: string;
 }
 
 interface SocialSectionProps {
-  title     : string;
-  skills    : Social[];
-  iconSize? : string;
+    title: string;
+    socials: Social[];
+    iconSize?: string;
 }
 
 const SocialSection: React.FC<SocialSectionProps> = ({
-  title,
-  skills,
-  iconSize = "w-8 sm:w-9 h-8 sm:h-9",
-}) => {
-  return (
-    <>
-      <h2 className="text-subtext1 text-xl font-semibold mb-4">
-        {title}
-      </h2>
-      <div className="flex flex-col items-center space-y-6 mb-6">
-        {skills.map((social, index) => (
-          <Icon
-            key={index}
-            Icon={social.Icon}
-            label={social.label}
-            color={social.color}
-            link={social.link}
-            iconSize={iconSize}
-          />
-        ))}
-      </div>
-    </>
-  );
+                                                         title,
+                                                         socials,
+                                                         iconSize = "w-8 sm:w-9 h-8 sm:h-9",
+                                                     }) => {
+    return (
+        <>
+            <h2 className="text-subtext1 text-xl font-semibold mb-4">
+                {title}
+            </h2>
+            <div className="flex flex-wrap justify-center space-x-4 mb-6">
+                {socials.map((social, index) => (
+                    <Icon
+                        key={index}
+                        Icon={social.Icon}
+                        label={social.label}
+                        color={social.color}
+                        link={social.link}
+                        iconSize={iconSize}
+                    />
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default SocialSection;
